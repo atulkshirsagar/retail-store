a4bf1077d35e539bfb3e57d6ec289416cbb9e47a
Atul Kshirsagar <atulkshirsagar@yahoo.com>
2015-08-03 12:28:43 -0700

    Initial Commit

---
+++ Retail Store/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Retail Store
+//
+//  Created by Atul Kshirsagar on 8/3/15.
+//  Copyright © 2015 Atul Kshirsagar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Retail StoreUITests/Retail_StoreUITests.swift
@@ -0,0 +1,35 @@
+//
+//  Retail_StoreUITests.swift
+//  Retail StoreUITests
+//
+//  Created by Atul Kshirsagar on 8/3/15.
+//  Copyright © 2015 Atul Kshirsagar. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class Retail_StoreUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
